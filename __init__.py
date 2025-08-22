d41d487099dd8e06de415e68b3249600f01eb68b
WordfromtheWise <drwhiet@spacefriends.de>
2025-08-22 16:34:24 +0200

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -2,10 +2,7 @@
 # Minimal package init to expose the whiet_node to ComfyUI
 
 from .whiet_node import WhietNode
-from .midi_node_01  import WhietMidi01Node
 
 # Optional: expose node mappings at package level
 NODE_CLASS_MAPPINGS = {"WhietNode": WhietNode}
 NODE_DISPLAY_NAME_MAPPINGS = {"WhietNode": "whiet"}
-NODE_CLASS_MAPPINGS = {"WhietMidi01Node": WhietMidi01Node}
-NODE_DISPLAY_NAME_MAPPINGS = {"WhietMidi01Node": "whiet"}
